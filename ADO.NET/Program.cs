b099badac69094d830a4e74f257f8113ad3e9fa3
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2025-03-22 22:37:04 +0500

    Insert DONE

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -69,8 +69,9 @@ namespace ADO.NET
 
 			//Connector.Select("*","Directors");
 			//Connector.Select("title,release_date,FORMATMESSAGE(N'%s %s',first_name,last_name)","Movies,Directors","director=director_id");
-			Connector.InsertDirector("George", "Martin");
-			
+			//Connector.InsertDirector("Brian", "De Palma");
+
+			Connector.InsertMovie("Terminator 5 - Genesis", "2015-06-22", "1");
 			Connector.SelectDirectors();
 			Connector.SelectMovies();
 		
